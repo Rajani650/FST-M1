ea411cbd831b86295ec2ee62bcee17dd7a82aba5
Rajani650 <rajani.k3@ibm.com>
2026-01-26 11:28:22 +0530

    Add files via upload

---
+++ Python/Activities/Activity21.py
@@ -0,0 +1,30 @@
+import pytest
+def test_addition():
+    num1 = 10
+    num2 = 20
+    sum = num1 + num2
+    assert sum == 30
+
+def test_subtraction():
+    num1 = 30
+    num2 = 20
+    diff = num1 - num2
+    assert diff ==10
+
+def test_multiplication():
+    num1 = 2
+    num2 = 5
+    mul = num1 * num2
+    assert mul == 10
+
+def test_division():
+    num1 = 10
+    num2 = 5
+    div = num1 / num2
+    assert div == 2
+
+
+
+
+
+    
