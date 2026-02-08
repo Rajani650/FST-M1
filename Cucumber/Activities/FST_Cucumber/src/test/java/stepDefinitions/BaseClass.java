c300f85aaae9db3a6039b240d6134ed8dbe2b9fc
Rajani650 <rajani.k3@ibm.com>
2026-02-09 00:02:14 +0530

    uploading cucumber activities
    uploading cucumber activities

---
+++ Cucumber/Activities/FST_Cucumber/src/test/java/stepDefinitions/BaseClass.java
@@ -0,0 +1,13 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.After;
+
+public class BaseClass {
+    // Create a new instance of the Firefox driver
+    static WebDriver driver;
+    static WebDriverWait wait;
+    
+}
